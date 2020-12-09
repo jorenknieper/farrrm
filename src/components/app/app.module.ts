d7dabf56410c2147a7cce653d6902dd446005884
cedricmarx <cedric.marx@ccvlab.eu>
2020-12-09 20:32:23 +0100

    Completely responsive text and navbar

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,7 +1,7 @@
 import { CollapseModule } from 'ngx-bootstrap/collapse';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, Router } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
