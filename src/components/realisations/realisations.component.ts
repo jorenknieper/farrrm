f827047446ade528210b2a61371cc1b669594cbd
cedricmarx <cedric.marx@ccvlab.eu>
2020-12-10 21:35:11 +0100

    Fix navbar and realisations page

--- src/components/realisations/realisations.component.ts
+++ src/components/realisations/realisations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-realisations',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./realisations.component.scss']
 })
 export class RealisationsComponent implements OnInit {
+  videos: any;
 
-  constructor() { }
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnInit(): void {
+    let descriptionPlaceholder = 'Tectatio berferspit desciis verum reri ommos ex eum quas vel ius eaquas aciti aut qui odis accum ad quia quam excea cum haribus quatium quod Mus. Puda vel et mod estotae volupta nempos et il in nimaximod ut poriatus asse si officia pos pa ipsam volupta nemporis eosti';
+    this.videos = [
+      { title: 'LATEST', url: this.getYouTubeUrlByEmbedId('iTHhMj7Z4n8'), description: descriptionPlaceholder, left: true },
+      { title: 'PUURR', url: this.getYouTubeUrlByEmbedId('iTHhMj7Z4n8'), description: descriptionPlaceholder, left: false },
+      { title: 'FARRM PRESENTS', url: this.getYouTubeUrlByEmbedId('iTHhMj7Z4n8'), description: descriptionPlaceholder, left: true },
+      { title: 'FARRM EXTENDED', url: this.getYouTubeUrlByEmbedId('iTHhMj7Z4n8'), description: descriptionPlaceholder, left: false },
+      { title: '22MINUTES', url: this.getYouTubeUrlByEmbedId('iTHhMj7Z4n8'), description: descriptionPlaceholder, left: true },
+    ];
   }
 
+  getYouTubeUrlByEmbedId(id: string) {
+    let unsafeUrl: string = 'https://www.youtube.com/embed/' + id;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(unsafeUrl); 
+  }
 }
