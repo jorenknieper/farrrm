04cd856baab8270547f21766d50efb96f4108dcb
cedricmarx <cedric.marx@ccvlab.eu>
2020-12-11 09:44:43 +0100

    Refactor about and realisations page

--- src/components/about/about.component.ts
+++ src/components/about/about.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  members: any = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.members = [
+      { name: 'MICHEL', description: 'Deze MANager. Da doluptat. Tendam fuga. Ut vit am, officidia nem dolor adisquat omnis volo quia conseque et, seque labore dolum voluptiis quas restrum nam, in nonsequi cus del ipsum et res inveni consequae voluptat. Bo. Cullut fugit mod', imageUri: 'assets/images/FARRM_Michel.jpg', left: true },
+      { name: 'JAN', description: 'Deze audio man Ficaepra tustibus mo is mi, omnimus aut etur? One non num quidelestrum ad qui autat ab intur alite nobitatem aut re ea porehentium repero blandi con resed moluptae. Nequid unda quam, officitius ad undita simpostias is aut dolore', imageUri: 'assets/images/FARRM_jan.jpg', left: false },
+      { name: 'ROBIN', description: 'Deze visuele man. Da doluptat. Tendam fuga. Ut vit am, officidia nem dolor adisquat omnis volo quia conseque et, seque labore dolum voluptiis quas restrum nam, in nonsequi cus del ipsum et res inveni consequae voluptat. Bo. Cullut fugit mod', imageUri: 'assets/images/FARRM_Michel.jpg', left: true },
+      { name: 'JOREN', description: 'Deze live man Ficaepra tustibus mo is mi, omnimus aut etur? One non num quidelestrum ad qui autat ab intur alite nobitatem aut re ea porehentium repero blandi con resed moluptae. Nequid unda quam, officitius ad undita simpostias is aut dolore', imageUri: 'assets/images/FARRM_jan.jpg', left: false },
+      { name: 'VRIJWILLIGERS', description: 'Deze gratis man. Da doluptat. Tendam fuga. Ut vit am, officidia nem dolor adisquat omnis volo quia conseque et, seque labore dolum voluptiis quas restrum nam, in nonsequi cus del ipsum et res inveni consequae voluptat. Bo. Cullut fugit mod', imageUri: 'assets/images/FARRM_Michel.jpg', left: true }
+    ];
   }
-
 }
